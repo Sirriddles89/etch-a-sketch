5133adc4e202acae6bcf003c64f05a05e21e9fc5
Oliver Motok <olivermotok@gmail.com>
2023-03-28 18:53:11 -0500

    Ensure user enters valid integer range, minor code cleanup. User must enter a number between 0 and 65 else the program will not run.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,6 +3,10 @@ function drawGrid(e) {
     gridContainer.innerHTML = "";
     let input = document.querySelector('#squares').value;
     squares = parseInt(input);
+    if (squares > 64 || squares < 1) {
+        alert("Please enter a number between 0 and 65");
+        return;
+    }
     e.preventDefault();
     let size = 960 / squares;
     for (let i = 0; i < squares; i++) {
@@ -22,13 +26,30 @@ function drawGrid(e) {
 function colorCells() {
     let cells = document.querySelectorAll(".gridBlock");
     cells.forEach((cell) => {
-        cell.addEventListener('mouseover', () => {
-            cell.style.backgroundColor = "blue";
-            });
+        cell.addEventListener('mouseover', () => {  
+            cell.style.backgroundColor = randomBackground();
+                      
         });
+    });
 }
 
+//refresh grid (this is an onclick function)
+function refresh() {
+    gridContainer.innerHTML = "";
+}
 
+function randomBackground() {
+    let x = Math.floor(Math.random() * 256)
+    let y = Math.floor(Math.random() * 256)
+    let z = Math.floor(Math.random() * 256)
+    let bgColor = `rgb(${x}, ${y}, ${z})`
+
+     return bgColor;
+
+    
+}
+
+let brightness = 100;
 const gridContainer = document.querySelector(".gridContainer");
 window.addEventListener('submit', drawGrid)
 
